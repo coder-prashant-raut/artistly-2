5891b56c1bce1d39d0eae7e5a0c32afa55e025b3
Prashant Raut <prashantraut1308@gmail.com>
2025-06-23 11:58:41 +0530

    Basic Project Initialized

---
+++ artistly/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div>
+  <div>
+    <h1 className="text-9xl">WElcome</h1>
+  </div>
+   </div>
+
+  );
+}
